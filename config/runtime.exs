7bb3338c703f9b795493a575d8d1d14598ba3e62
skyeto <mail@skyeto.com>
2025-04-12 12:56:29 +0200

    add runtime config

---
+++ config/runtime.exs
@@ -0,0 +1,31 @@
+import Config
+
+config :streamer, ecto_repos: [Streamer.Repo]
+
+config :streamer, Streamer.Repo,
+  database: "streamer",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
+
+config :ex_aws,
+  access_key_id: System.get_env("STREAMER_S3_ACCESS_KEY"),
+  secret_access_key: System.get_env("STREAMER_S3_SECRET_ACCESS_KEY")
+
+config :ex_aws, :s3,
+  scheme: "https://",
+  host: "data.skyeto.net"
+
+config :streamer, StreamerWeb.Endpoint,
+  adapter: Bandit.PhoenixAdapter,
+  url: [host: "api.blog.skyeto.net", port: 443],
+  secret_key_base: System.get_env("STREAMER_SECRET_KEY_BASE"),
+  http: [port: 3000],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  pubsub_server: Streamer.PubSub
+
+config :streamer, Streamer.Guardian.Session,
+  issues: "streamer",
+  secret_key: System.get_env("STREAMER_GUARDIAN_SESSION_SECRET")
--- lib/streamer/release.ex
+++ lib/streamer/release.ex
@@ -2,6 +2,8 @@ defmodule Streamer.Release do
   @app :streamer
 
   def migrate() do
+    load_app()
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
@@ -9,5 +11,9 @@ defmodule Streamer.Release do
 
   defp repos() do
     Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app() do
+    Application.load(@app)
   end
 end
